a1cd9b1a19c88c870fa4e1ccd0427a7dc76340d7
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-06-29 18:54:57 -0400

    Agrego la opcion de buscar serie por titulo en repositorio

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -32,6 +32,7 @@ public class Principal {
                     1 - Buscar series 
                     2 - Buscar episodios
                     3 - Mostrar series buscadas
+                    4 - Buscar serie por titulo
                                   
                     0 - Salir
                     """;
@@ -48,6 +49,9 @@ public class Principal {
                     break;
                 case 3:
                     mostrarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
                     break;
                 case 0:
                     System.out.println("Cerrando la aplicación...");
@@ -123,6 +127,18 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo(){
+        System.out.println("Escribe el nombre de la seria que deseas buscar: ");
+        var nombreSerie = teclado.nextLine();
+
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainsIgnoreCase(nombreSerie);
+        if (serieBuscada.isPresent()){
+            System.out.println("La serie buscada es: " + serieBuscada.get());
+        }else{
+            System.out.println("Serie no encontrada");
+        }
     }
 }
 
